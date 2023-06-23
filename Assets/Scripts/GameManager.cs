398cbb1c7bcc0de98b2441fa7501112f2657b655
mouraa45 <126695434+mouraa45@users.noreply.github.com>
2023-06-23 14:51:28 +0100

    terminado
    botoes modos de jogo etc

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,63 +7,78 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public List<GameObject> targets;
-    public TextMeshProUGUI scoreText;
-    public TextMeshProUGUI gameOverText;
-    public bool isGameActive;
-    public Button restartButton;
-    private float spawnRate = 1.0f;
-    private int score;
+	public List<GameObject> targets;
+	public TextMeshProUGUI scoreText;
+	public TextMeshProUGUI gameOverText;
+	public bool isGameActive;
+	public Button restartButton;
+	public GameObject titleScreen;
+	private float spawnRate = 1.0f;
+	private int score;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-    	isGameActive=true;
-    	 score = 0;
-    	 
-        StartCoroutine(SpawnTarget());
-        UpdateScore(0);
-        
-    }
+	// Start is called before the first frame update
+	void Start()
+	{
+		isGameActive=true;
+		score = 0;
+		
+		StartCoroutine(SpawnTarget());
+		UpdateScore(0);
+		
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
+	// Update is called once per frame
+	void Update()
+	{
 
-    }
+	}
 
-    IEnumerator SpawnTarget()
-{
-    while (isGameActive)
-    {
-    	yield return new WaitForSeconds(spawnRate);
+	IEnumerator SpawnTarget()
+	{
+		while (isGameActive)
+		{
+			yield return new WaitForSeconds(spawnRate);
 
-        if (targets.Count > 0)
-        {
-            int index = Random.Range(0, targets.Count);
-            Instantiate(targets[index]);
-            
-        }
-    }
-}
+			if (targets.Count > 0)
+			{
+				int index = Random.Range(0, targets.Count);
+				Instantiate(targets[index]);
+				
+			}
+		}
+	}
 
-    public void UpdateScore(int scoreToAdd)
-    {
-        score += scoreToAdd;
-        scoreText.text = "Score: " + score;
-    }
-    
-    public void GameOver()
-    {
-    	restartButton.gameObject.SetActive(true);
-    	gameOverText.gameObject.SetActive(true);
-    	isGameActive=false;
-    	
-    }
-    
-    public void Restartgame()
-    {
-    	
-    	SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+	public void UpdateScore(int scoreToAdd)
+	{
+		score += scoreToAdd;
+		scoreText.text = "Score: " + score;
+	}
+	
+	public void GameOver()
+	{
+		restartButton.gameObject.SetActive(true);
+		gameOverText.gameObject.SetActive(true);
+		isGameActive=false;
+		
+	}
+	
+	public void Restartgame()
+	{
+		
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+	
+	
+	public void StartGame(int difficulty)
+	{
+		
+		isGameActive=true;
+		score = 0;
+		spawnRate=spawnRate/ difficulty;
+		StartCoroutine(SpawnTarget());
+		UpdateScore(0);
+		titleScreen.gameObject.SetActive(false);
+		
+	}
 }
